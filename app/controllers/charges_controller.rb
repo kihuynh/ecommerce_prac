2c3248ac4789067fdaf10218e97109e2561053cd
roger yee <ryee.epicodus@gmail.com>
2018-05-02 09:40:06 -0700

    adds api keys to env

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,31 @@
 class ChargesController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer => customer.id,
+      :amount => @amount,
+      :description => 'Practice Stripe',
+      :currency => 'usd'
+    )
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+  end
+
+  def show
+    @order_items = current_order.order_items
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   end
 
   resources :products
+
+  resources :charges
 end
