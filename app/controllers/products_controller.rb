72c46bcf95f1aa6a3c152062a891191f492f2d7e
roger yee <ryee.epicodus@gmail.com>
2018-05-02 09:22:51 -0700

    formats cart page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,5 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
-
-    walmart_item = Walmart.new("poptart")
-    @salePrice = walmart_item.get_item_price()
   end
 end
--- app/services/get_bestsellers.rb
+++
@@ -1,6 +0,0 @@
-class Bestseller
-  def self.get_bestseller_list
-    response = RestClient::Request.execute(method: :get, url: 'https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json', headers: {api_key: ['NYT_API_KEY']})
-    JSON.parse(response)["results"]["books"]
-  end
-end
